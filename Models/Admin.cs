5ca3a14466e4933b16ad793cfe371dd39d17be40
temp <duylinhsss@hotmail.com>
2022-08-25 18:56:37 +1000

    Add more models

---
+++ Models/Admin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace _272Ass3.Models
+{
+    public class Admin : User
+    {
+        public Admin()
+        {
+        }
+    }
+}
+
---
+++ Models/Organiser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace _272Ass3.Models
+{
+    public class Organiser : User
+    {
+        public Organiser()
+        {
+        }
+    }
+}
+
